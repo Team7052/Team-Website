9888c5cf7fbc7bd1f061918e99eabf97c04f4208
CEWL <caleb.lammers@tbcschools.ca>
2018-05-17 20:01:04 -0400

    Added info and box for a picture to The Team page

--- about/The Team.php
+++ about/The Team.php
@@ -1,17 +1,35 @@
+<?php session_start(); ?>
+<!DOCTYPE HTML>
+<html>
+<head>
 <link type="text/css" rel="stylesheet" href="../stylesheets/about/aboutTheTeamStyle.css">
+<link href="stylesheets/global/footerStyle.css" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900" rel="stylesheet">
+<link href="stylesheets/homeStyle.css" rel="stylesheet" type="text/css">
+<link href="stylesheets/global/navbarStyle.css" rel="stylesheet" type="text/css">
+<link href="stylesheets/global/footerStyle.css" rel="stylesheet" type="text/css">
+<link href="stylesheets/global/globalStyle.css" rel="stylesheet" type="text/css">
+<title>About | Team 7052</title>
+</head>
+
 
 <div id="about-team-paragraph-section">
+  <div id="first-image"></div>
   <p>
-    quisquam est, qui dolorem ipsum quia
-    dolor sit amet, consectetur, adipisci
-    velit, sed quia non numquam eius modi
-    tempora incidunt ut labore et dolore
-    magnam aliquam quaerat voluptatem.
-    Ut enim ad minima veniam, quis
-    nostrum exercitationem ullam corporis
-    suscipit laboriosam, nisi ut aliquid ex ea
-    commodi consequatur? Quis autem vel
-    eum iure reprehenderit qui in ea
-    voluptate velit esse quam nihil
+  We are the St. Ignatius High School Robotics Team (Falcotronix)
+ consisting of many enthusiastic students led by several dedicated
+ teachers at our school. The goal of Falcotronix is to promote
+ STEM and FIRST Robotics not only in our school community, but in
+ the city of Thunder Bay. Our team helps students develop
+ essential skills for the future in both academics and leadership.
+ Our devoted and experienced mentors teach students new skills
+ in leadership, marketing, engineering, communication, and critical
+ thinking. As a newly formed team, we strive to continue to succeed
+ FIRST Robotics evets and in time form a well established Robotics program
+ in our school.
   </p>
 </div>
+<?php
+include("templateFiles/footer.html");
+?>
+</html>
