385aed0eaec22f4a7ff8d6467fab7944e3deb6d8
CEWL <caleb.lammers@tbcschools.ca>
2018-05-20 19:32:19 -0400

    Added Media Social page

--- media/social.php
+++ media/social.php
@@ -1,3 +1,32 @@
-<div>
-  Social
+<link type="text/css" rel="stylesheet" href="../stylesheets/media/socialStyle.css">
+
+<div id="media-section">
+  <img id="facebookLogo" src="../public/images/socialMediaLogos/facebook.png">
+  <img id="instagramLogo" src="../public/images/socialMediaLogos/instagram.png">
+  <img id="youtubeLogo" src="../public/images/socialMediaLogos/youtube.png">
+
+  <div id="facebookLogo">
+    <h2> Facebook <h2>
+  </div>
+
+  <div id="facebookLogo">
+    <h3> Find all events <h3>
+  </div>
+
+  <div id="instagramLogo">
+    <h2> Instagram <h2>
+  </div>
+
+  <div id="instagramLogo">
+    <h3> Follow us <h3>
+  </div>
+
+  <div id="youtubeLogo">
+    <h2> Youtube <h2>
+  </div>
+
+  <div id="youtubeLogo">
+    <h3> All our videos <h3>
+  </div>
+
 </div>
