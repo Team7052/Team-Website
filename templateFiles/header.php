eb29bd79fe38c5ca4c061480bbf998567dbfcd6b
kevinbai0 <kevin.c.bai0@gmail.com>
2018-05-23 19:57:08 -0400

    About page, and page template is mobil responsive

--- templateFiles/header.php
+++ templateFiles/header.php
@@ -1,4 +1,5 @@
 <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,900" rel="stylesheet">
   <link href="../stylesheets/global/navbarStyle.css" rel="stylesheet" type="text/css">
   <link href="../stylesheets/global/footerStyle.css" rel="stylesheet" type="text/css">
